9123c0d1c6dd2cbafea9493a2449757b0380b63d
fkwp <github-fkwp@w4ve.de>
2025-03-12 08:58:54 +0100

    more idomatic variable nameing

--- main_test.go
+++ main_test.go
@@ -107,8 +107,8 @@ func TestHandlePost(t *testing.T) {
   handler := &Handler{
     secret: "testSecret",
     key: "testKey",
-    lk_url: "wss://lk.local:8080/foo",
-    local_homeservers: []string{"example.com"},
+    lkUrl: "wss://lk.local:8080/foo",
+    localHomeservers: []string{"example.com"},
     skipVerifyTLS: true,
   }
 
@@ -212,18 +212,18 @@ func TestGetJoinToken(t *testing.T) {
   room := "testRoom"
   identity := "testIdentity@example.com"
 
-  for _, is_local_user := range []bool{true, false} {
-    token_string, err := getJoinToken(is_local_user, apiKey, apiSecret, room, identity)
+  for _, isLocalUser := range []bool{true, false} {
+    tokenString, err := getJoinToken(isLocalUser, apiKey, apiSecret, room, identity)
     if err != nil {
       t.Fatalf("unexpected error: %v", err)
     }
 
-    if token_string == "" {
+    if tokenString == "" {
       t.Error("expected token to be non-empty")
     }
 
     // parse JWT checking the shared secret
-    token, err := jwt.Parse(token_string, func(token *jwt.Token) (interface{}, error) {
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
       return []byte(apiSecret), nil
     })
     claims, ok := token.Claims.(jwt.MapClaims)
@@ -232,13 +232,13 @@ func TestGetJoinToken(t *testing.T) {
       t.Fatalf("failed to parse claims from JWT: %v", err)
     }
 
-    claim_room_create := claims["video"].(map[string]interface{})["roomCreate"]
-    if claim_room_create == nil {
-      claim_room_create = false
+    claimRoomCreate := claims["video"].(map[string]interface{})["roomCreate"]
+    if claimRoomCreate == nil {
+      claimRoomCreate = false
     }
 
-    if is_local_user != claim_room_create {
-      t.Fatalf("roomCreate propery does not reflect is_local_user intent")
+    if isLocalUser != claimRoomCreate {
+      t.Fatalf("roomCreate property does not reflect isLocalUser intent")
     }
     
   }
