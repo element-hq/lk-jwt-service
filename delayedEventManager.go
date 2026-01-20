5721c998695af76550f493188243d795c8dbe15b
fkwp <github-fkwp@w4ve.de>
2026-01-20 21:58:33 +0100

    fix: update log message for delayed disconnect event error

--- delayedEventManager.go
+++ delayedEventManager.go
@@ -509,7 +509,7 @@ func (m *LiveKitRoomMonitor) AddJob(name LiveKitIdentity, job *DelayedEventJob) 
         )
 
         if err != nil {
-            slog.Warn("Error while issuing delayed disconnect event <restart> action", "room", job.LiveKitRoom, "lkId", job.LiveKitIdentity, "err", err)
+            slog.Warn("Error while looking up LiveKit participant", "room", job.LiveKitRoom, "lkId", job.LiveKitIdentity, "err", err)
         }
     }()
 
