324fdc8be80fd6181b9bd10bc7f1abbced519c7a
Koen Oostveen <koen@koenoostveen.nl>
2026-08-25 10:09:18 +0200

    Add unit test for healthcheck

--- src/bin/healthcheck.rs
+++ src/bin/healthcheck.rs
@@ -8,13 +8,18 @@
 
 use lk_jwt_service::config::{bind_addresses, parse_bind};
 
+#[inline]
+fn get_healthz_url(host_and_port: &str) -> String {
+    format!("http://{host_and_port}/healthz")
+}
+
 #[tokio::main]
 async fn main() -> Result<(), String> {
     let lk_jwt_bind = parse_bind()?;
     let addrs = bind_addresses(&lk_jwt_bind);
 
     for addr in addrs {
-        let resp = match reqwest::get(format!("http://{addr}/healthz")).await {
+        let resp = match reqwest::get(get_healthz_url(&addr)).await {
             Ok(resp) => resp,
             Err(err) => {
                 println!("Connection error: {err}");
@@ -32,4 +37,46 @@ async fn main() -> Result<(), String> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use url::Url;
+
+    fn validate_url(url_str: &str, expected_host: &str, expected_port: u16) {
+        let url_res = Url::parse(url_str);
+        assert!(url_res.is_ok());
+
+        let url = url_res.unwrap();
+        assert_eq!(url.host_str().unwrap_or("0.0.0.0"), expected_host);
+        assert_eq!(url.port_or_known_default().unwrap_or_default(), expected_port);
+    }
+
+    fn test_healthz_url(lk_jwt_bind: &str, expected_host: &str, expected_port: u16) {
+        let addrs = bind_addresses(lk_jwt_bind);
+        let last = addrs.last().expect("bind_addresses returned empty Vec");
+        let url_str = get_healthz_url(last);
+        validate_url(&url_str, expected_host, expected_port);
+    }
+
+    #[test]
+    fn bind_all_valid() {
+        test_healthz_url(":8080", "0.0.0.0", 8080);
+        test_healthz_url(":1234", "0.0.0.0", 1234);
+        test_healthz_url(":443", "0.0.0.0", 443);
+    }
+
+    #[test]
+    fn bind_specific_valid() {
+        test_healthz_url("127.0.0.1:8080", "127.0.0.1", 8080);
+        test_healthz_url("127.0.0.1:443", "127.0.0.1", 443);
+    }
+    
+    #[test]
+    fn bind_all_invalid() {
+        let addrs = bind_addresses("8080");
+        let last = addrs.last().expect("bind_addresses returned empty Vec");
+        assert!(Url::parse(last).is_err());
+    }
 }
